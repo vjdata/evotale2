1baa09f21f3726b4b6a42fb055d58cd6b5531d40
Viktor Johansson <viktor@vjdata.se>
2018-09-23 12:36:26 +0200

    Music

---
+++ Evotale2/Assets/Scripts/DevolverScript_Rat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+public class DevolverScript_Rat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.GetComponent<Player>().DevolveRat();
+
+            var x = other.GetComponent<CapsuleCollider>();
+            x.height = 1.85f;
+            x.radius = 0.29f;
+            x.direction = 2;
+            x.center = new Vector3(0, 0.24f, 0);
+
+        }
+    }
+}
--- Evotale2/Assets/Scripts/Player.cs
+++ Evotale2/Assets/Scripts/Player.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-    public GameObject LeOgre;
-
     public GameObject Ethan;
+    public GameObject LeOgre;
+    public GameObject Rat;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -22,5 +22,11 @@ public class Player : MonoBehaviour
     {
         this.Ethan.SetActive(false);
         this.LeOgre.SetActive(true);
+    }
+
+    public void DevolveRat()
+    {
+        this.LeOgre.SetActive(false);
+        this.Rat.SetActive(true);
     }
 }
